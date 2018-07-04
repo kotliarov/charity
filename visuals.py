f4323eeef3ad224c161b631b39167d58a5db1986
Alex Kotliarov <alexander.kotliarov@gmail.com>
2018-07-04 02:18:30 -0400

    Layman description

--- visuals.py
+++ visuals.py
@@ -61,7 +61,7 @@ def evaluate(results, accuracy, f1):
   
     # Create figure
     fig, ax = pl.subplots(2, 3, figsize = (11,7))
-
+    pl.tight_layout()
     # Constants
     bar_width = 0.3
     colors = ['#A00000','#00A0A0','#00A000']
